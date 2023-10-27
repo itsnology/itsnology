309b59851deae785bae460739dac616fd4f9323d
Abdou-MTR <metiriabdou@gmail.com>
2023-10-27 12:26:15 +0200

    first commit navbar changes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
-
+import Navbar from "@components/navbar";
 export default function Home() {
-   return <main className=" bggradient ">hello there hahaha </main>;
+  return (
+    <main className=" bggradient ">
+      <Navbar />
+    </main>
+  );
 }
