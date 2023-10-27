eed93acfe607d29a83f5d3149e2d46b33694d763
Mouad Kahla <mouadmire8@gmail.com>
2023-10-27 09:45:33 +0100

    test

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-   return <main className=" bggradient "></main>;
+   return <main className=" bggradient ">hello there </main>;
 }
