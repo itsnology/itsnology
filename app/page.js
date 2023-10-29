275175491e838e9d75a095842dc29baf1825a2e3
Mouad Kahla <mouadmire8@gmail.com>
2023-10-29 10:04:49 +0100

    fixed issues

--- app/page.js
+++ app/page.js
@@ -9,19 +9,15 @@ import Rating from "@components/rating";
 import FirstDisc from "@components/firstDisc";
 import Popup from "@components/popup";
 export default function Home() {
-<<<<<<< HEAD
-   return <main className=" bggradient ">hello there </main>;
-=======
-  return (
-    <main className=" ">
-      <Navbar />
-      <Slider />
-      <Popup />
-      <FirstDisc />
-      <FirstPage2 />
-      <MenuService />
-      <Rating />
-    </main>
-  );
->>>>>>> d89b25599639078d7d14b06ea2e606d6251fb884
+   return (
+      <main className=" ">
+         <Navbar />
+         <Slider />
+         <Popup />
+         <FirstDisc />
+         <FirstPage2 />
+         <MenuService />
+         <Rating />
+      </main>
+   );
 }
