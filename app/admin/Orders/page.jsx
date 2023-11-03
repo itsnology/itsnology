545d10d2b27c39f6b6688ffb34d5739164df365c
Mouad Kahla <mouadmire8@gmail.com>
2023-11-03 11:02:27 +0100

    admin modifications

--- app/admin/Orders/page.jsx
+++ app/admin/Orders/page.jsx
@@ -1,11 +1,177 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
 import SideBar from "@components/sidebar";
-const page = () => {
-  return (
-    <div>
-      <SideBar />
-    </div>
-  );
+
+const Orders = () => {
+   const [expandedRow, setExpandedRow] = useState(null);
+
+   const orders = [
+      {
+         id: 1,
+         userId: 1,
+         productName: "منتج 1",
+         date: "2022-01-01",
+         time: "10:00 AM",
+         price: 100,
+         serviceName: "خدمة 1",
+         userName: "جون دو",
+         category: "cards",
+         done: false,
+      },
+      {
+         id: 2,
+         userId: 1,
+         productName: "منتج 2",
+         date: "2022-01-02",
+         time: "11:00 AM",
+         price: 200,
+         serviceName: "خدمة 2",
+         userName: "جون دو",
+         category: "list",
+         done: false,
+      },
+      {
+         id: 3,
+         userId: 2,
+         productName: "منتج 3",
+         date: "2022-01-03",
+         time: "12:00 PM",
+         price: 300,
+         serviceName: "خدمة 3",
+         userName: "جين دو",
+         category: "cards",
+         done: true,
+      },
+      {
+         id: 4,
+         userId: 2,
+         productName: "منتج 4",
+         date: "2022-01-04",
+         time: "01:00 PM",
+         price: 400,
+         serviceName: "خدمة 4",
+         userName: "جين دو",
+         category: "list",
+         done: false,
+      },
+   ].sort((a, b) => {
+      const dateA = new Date(`${a.date} ${a.time}`);
+      const dateB = new Date(`${b.date} ${b.time}`);
+      return dateB - dateA;
+   });
+
+   const [Orders, setOreders] = useState(orders);
+
+   const users = [
+      {
+         id: 1,
+         name: "جون دو",
+         email: "johndoe@example.com",
+         number: "123-456-7890",
+      },
+      {
+         id: 2,
+         name: "جين دو",
+         email: "janedoe@example.com",
+         number: "987-654-3210",
+      },
+   ];
+
+   const handleRowClick = (id) => {
+      if (expandedRow === id) {
+         setExpandedRow(null);
+      } else {
+         setExpandedRow(id);
+      }
+   };
+
+   const handleCheckboxClick = (e, id) => {
+      e.stopPropagation();
+      const updatedOrders = orders.map((order) => {
+         if (order.id === id) {
+            return {
+               ...order,
+               done: !order.done,
+            };
+         }
+         return order;
+      });
+
+      setOreders(updatedOrders);
+   };
+
+   return (
+      <div className="flex md:flex-row ">
+         <SideBar />
+         <div className="flex-grow p-4">
+            <table className="table-auto w-full">
+               <thead>
+                  <tr>
+                     <th className="px-4 py-2">المستخدم</th>
+                     <th className="px-4 py-2">اسم المنتج</th>
+                     <th className="px-4 py-2">الخدمة</th>
+                     <th className="px-4 py-2">التاريخ</th>
+                     <th className="px-4 py-2">الوقت</th>
+                     <th className="px-4 py-2">السعر</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {orders.map((order) => (
+                     <React.Fragment key={order.id}>
+                        <tr
+                           className={`border-b border-gray-200 cursor-pointer ${
+                              expandedRow === order.id
+                                 ? "bg-sky-200 font-bold "
+                                 : ""
+                           } ${order.done ? "bg-blue-200" : ""} ${
+                              order.category === "cards" ? "bg-rose-200" : ""
+                           }`}
+                           onClick={() => handleRowClick(order.id)}
+                        >
+                           <td className="px-4 py-2">
+                              <Link
+                                 href={`#${order.userId}`}
+                                 className="text-blue-500 hover:underline"
+                              >
+                                 {order.userName}
+                              </Link>
+                           </td>
+                           <td className="px-4 py-2">
+                              <Link
+                                 href={`/services/${order.productName}`}
+                                 className="text-blue-500 hover:underline"
+                              >
+                                 {order.productName}
+                              </Link>
+                           </td>
+                           <td className="px-4 py-2">
+                              <Link href={`/services/${order.productName}`}>
+                                 {order.productName}
+                              </Link>
+                           </td>
+                           <td className="px-4 py-2">{order.date}</td>
+                           <td className="px-4 py-2">{order.time}</td>
+                           <td className="px-4 py-2">{order.price}</td>
+                           {order.category !== "cards" && (
+                              <td className="px-4 py-2">
+                                 <input
+                                    type="checkbox"
+                                    checked={order.done}
+                                    onChange={(e) =>
+                                       handleCheckboxClick(e, order.id)
+                                    }
+                                 />
+                              </td>
+                           )}
+                        </tr>
+                     </React.Fragment>
+                  ))}
+               </tbody>
+            </table>
+         </div>
+      </div>
+   );
 };
 
-export default page;
+export default Orders;
--- app/admin/users/page.jsx
+++ app/admin/users/page.jsx
@@ -1,80 +1,141 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import SideBar from "@components/sidebar";
-import PocketBase from "pocketbase";
 
-const pb = new PocketBase("http://127.0.0.1:8090");
-const records = await pb.collection("users").getFullList({
-  sort: "-created",
-});
+const Users = () => {
+   const [expandedRow, setExpandedRow] = useState(null);
+   const users = [
+      {
+         id: 1,
+         name: "جون دو",
+         email: "johndoe@example.com",
+         number: "123-456-7890",
+         orders: [
+            {
+               id: 1,
+               name: "الطلب 1",
+               type: "النوع 1",
+               date: "2022-01-01",
+               price: 100,
+            },
+            {
+               id: 2,
+               name: "الطلب 2",
+               type: "النوع 2",
+               date: "2022-01-02",
+               price: 200,
+            },
+         ],
+      },
+      {
+         id: 2,
+         name: "جين دو",
+         email: "janedoe@example.com",
+         number: "987-654-3210",
+         orders: [
+            {
+               id: 3,
+               name: "الطلب 3",
+               type: "النوع 3",
+               date: "2022-01-03",
+               price: 300,
+            },
+            {
+               id: 4,
+               name: "الطلب 4",
+               type: "النوع 4",
+               date: "2022-01-04",
+               price: 400,
+            },
+         ],
+      },
+   ];
 
-console.log(records);
+   const handleRowClick = (id) => {
+      if (expandedRow === id) {
+         setExpandedRow(null);
+      } else {
+         setExpandedRow(id);
+      }
+   };
 
-const users = () => {
-  const tableItems = [
-    {
-      name: "ليام جيمس",
-      email: "liamjames@example.com",
-      position: "مهندس برمجيات",
-      salary: "$100K",
-    },
-    {
-      name: "أوليفيا إيما",
-      email: "oliviaemma@example.com",
-      position: "مصمم المنتج",
-      salary: "$90K",
-    },
-    {
-      name: "وليام بنيامين",
-      email: "william.benjamin@example.com",
-      position: "مطور الواجهة الأمامية",
-      salary: "$80K",
-    },
-    {
-      name: "هنري ثيودور",
-      email: "henrytheodore@example.com",
-      position: "مهندس Laravel",
-      salary: "$120K",
-    },
-    {
-      name: "اميليا ايليا",
-      email: "amelia.elijah@example.com",
-      position: "مدير Open source",
-      salary: "$75K",
-    },
-  ];
-
-  return (
-    <div className="flex md:flex-row ">
-      <SideBar />
-      <div className="max-w-screen-lg basis-1/2 justify-end mx-auto md:mt-6 lg:mt-6 mt-12 ">
-        <div className="max-w-lg">
-          <h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
-            Users Informations
-          </h3>
-        </div>
-        <div className="mt-12 shadow-sm border rounded-lg overflow-x-auto">
-          <table className="w-full table-auto text-sm text-right">
-            <thead className="bg-gray-50 text-gray-600 font-medium border-b">
-              <tr>
-                <th className="py-3 px-6">Name</th>
-                <th className="py-3 px-6">Email</th>
-              </tr>
-            </thead>
-            <tbody className="text-gray-600 divide-y">
-              {records.map((item, idx) => (
-                <tr key={idx}>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    {item.username}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">{item.email}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+   return (
+      <div className="flex md:flex-row ">
+         <SideBar />
+         <div className="flex-grow p-4">
+            <table className="table-auto w-full">
+               <thead>
+                  <tr>
+                     <th className="px-4 py-2">الاسم</th>
+                     <th className="px-4 py-2">البريد الإلكتروني</th>
+                     <th className="px-4 py-2">رقم الهاتف</th>
+                     <th className="px-4 py-2">الطلبات</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {users.map((user) => (
+                     <React.Fragment key={user.id} id={user.id}>
+                        <tr
+                           className={`border-b border-gray-200 cursor-pointer ${
+                              expandedRow === user.id
+                                 ? "bg-sky-200 font-bold "
+                                 : ""
+                           }`}
+                           onClick={() => handleRowClick(user.id)}
+                        >
+                           <td className="px-4 py-2">{user.name}</td>
+                           <td className="px-4 py-2">{user.email}</td>
+                           <td className="px-4 py-2">{user.number}</td>
+                           <td className="px-4 py-2">
+                              <button
+                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                 onClick={() => handleRowClick(user.id)}
+                              >
+                                 عرض الطلبات لـ {user.name}
+                              </button>
+                           </td>
+                        </tr>
+                        {expandedRow === user.id && (
+                           <tr>
+                              <td colSpan="4">
+                                 <table className="table-auto w-full">
+                                    <thead>
+                                       <tr>
+                                          <th className="px-4 py-2">الاسم</th>
+                                          <th className="px-4 py-2">النوع</th>
+                                          <th className="px-4 py-2">التاريخ</th>
+                                          <th className="px-4 py-2">السعر</th>
+                                       </tr>
+                                    </thead>
+                                    <tbody>
+                                       {user.orders.map((order) => (
+                                          <tr key={order.id}>
+                                             <td className="px-4 py-2">
+                                                {order.name}
+                                             </td>
+                                             <td className="px-4 py-2">
+                                                {order.type}
+                                             </td>
+                                             <td className="px-4 py-2">
+                                                {order.date}
+                                             </td>
+                                             <td className="px-4 py-2">
+                                                {order.price}
+                                             </td>
+                                          </tr>
+                                       ))}
+                                    </tbody>
+                                 </table>
+                              </td>
+                           </tr>
+                        )}
+                     </React.Fragment>
+                  ))}
+               </tbody>
+            </table>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
-export default users;
+export default Users;
