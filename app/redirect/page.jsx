61b45bf568cbbc2e4c6d39a33012e5f54b921236
Mouad Kahla <mouadmire8@gmail.com>
2023-11-10 23:14:20 +0100

    lool

--- app/redirect/page.jsx
+++ app/redirect/page.jsx
@@ -1,21 +1,21 @@
-"use client";
-import { useEffect } from "react";
-import { GoSell } from "@tap-payments/gosell";
+// "use client";
+// import { useEffect } from "react";
+// import { GoSell } from "@tap-payments/gosell";
 
-const Redirect = () => {
-   useEffect(() => {
-      GoSell.showResult({
-         callback: (response) => {
-            console.log("callback", response);
-         },
-      });
-   }, []);
+// const Redirect = () => {
+//    useEffect(() => {
+//       GoSell.showResult({
+//          callback: (response) => {
+//             console.log("callback", response);
+//          },
+//       });
+//    }, []);
 
-   return (
-      <div className="App">
-         <GoSell />
-      </div>
-   );
-};
+//    return (
+//       <div className="App">
+//          <GoSell />
+//       </div>
+//    );
+// };
 
-export default Redirect;
+// export default Redirect;
