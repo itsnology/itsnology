fc1fb445a8825caa8611ecb25c12ae568c2cec02
Mouad Kahla <mouadmire8@gmail.com>
2023-11-10 22:54:04 +0100

    lol

--- app/admin/Orders/page.jsx
+++ app/admin/Orders/page.jsx
@@ -5,181 +5,196 @@ import SideBar from "@components/sidebar";
 import Adminlogin from "@components/adminlogin";
 
 const Orders = () => {
-  const [expandedRow, setExpandedRow] = useState(null);
-  const [OrderData, setOrderData] = useState([]);
-  const [OrderSocial, setOrderSocial] = useState([]);
-  const [loggedIn, setLoggedIn] = useState(false);
+   const [expandedRow, setExpandedRow] = useState(null);
+   const [OrderData, setOrderData] = useState([]);
+   const [OrderSocial, setOrderSocial] = useState([]);
+   const [loggedIn, setLoggedIn] = useState(false);
 
-  const submitLogin = async (e) => {
-    setLoggedIn(true);
-  };
+   const submitLogin = async (e) => {
+      setLoggedIn(true);
+   };
 
-  console.log(OrderSocial);
+   console.log(OrderSocial);
 
-  useEffect(() => {
-    const fetchOrders = async () => {
-      try {
-        const response = await fetch("/api/GetUser/GetOrder", {
-          cache: "no-store",
-        });
-        if (response.ok) {
-          const data = await response.json();
+   useEffect(() => {
+      const fetchOrders = async () => {
+         try {
+            const response = await fetch("/api/GetUser/GetOrder", {
+               cache: "no-store",
+            });
+            if (response.ok) {
+               const data = await response.json();
 
-          setOrderData(data);
-        } else {
-          console.error("Failed to fetch reviews");
-        }
-      } catch (error) {
-        console.error("Error fetching reviews:", error);
+               setOrderData(data);
+            } else {
+               console.error("Failed to fetch reviews");
+            }
+         } catch (error) {
+            console.error("Error fetching reviews:", error);
+         }
+      };
+      fetchOrders();
+   }, []);
+
+   useEffect(() => {
+      const fetchOrders = async () => {
+         try {
+            const response = await fetch("/api/GetUser/GetOrderSocial", {
+               cache: "no-store",
+            });
+            if (response.ok) {
+               const data = await response.json();
+
+               setOrderSocial(data);
+            } else {
+               console.error("Failed to fetch reviews");
+            }
+         } catch (error) {
+            console.error("Error fetching reviews:", error);
+         }
+      };
+      fetchOrders();
+   }, []);
+
+   const handleRowClick = (id) => {
+      if (expandedRow === id) {
+         setExpandedRow(null);
+      } else {
+         setExpandedRow(id);
       }
-    };
-    fetchOrders();
-  }, []);
+   };
 
-  useEffect(() => {
-    const fetchOrders = async () => {
-      try {
-        const response = await fetch("/api/GetUser/GetOrderSocial", {
-          cache: "no-store",
-        });
-        if (response.ok) {
-          const data = await response.json();
+   const handleCheckboxClick = (e, id) => {
+      e.stopPropagation();
+      const updatedOrders = OrderSocial.map((order) => {
+         if (order._id === id) {
+            return {
+               ...order,
+               done: !order.done,
+            };
+         }
+         return order;
+      });
 
-          setOrderSocial(data);
-        } else {
-          console.error("Failed to fetch reviews");
-        }
-      } catch (error) {
-        console.error("Error fetching reviews:", error);
-      }
-    };
-    fetchOrders();
-  }, []);
+      setOrderSocial(updatedOrders);
+   };
 
-  const handleRowClick = (id) => {
-    if (expandedRow === id) {
-      setExpandedRow(null);
-    } else {
-      setExpandedRow(id);
-    }
-  };
+   const Orders = [...OrderData, ...OrderSocial];
+   const sortedOrders = Orders.sort((a, b) => {
+      const aTime =
+         OrderData.find((order) => order._id === a._id)?.createdTime ||
+         OrderSocial.find((order) => order._id === a._id)?.createdTime;
+      const bTime =
+         OrderData.find((order) => order._id === b._id)?.createdTime ||
+         OrderSocial.find((order) => order._id === b._id)?.createdTime;
+      return new Date(bTime) - new Date(aTime);
+   });
 
-  const handleCheckboxClick = (e, id) => {
-    e.stopPropagation();
-    const updatedOrders = OrderSocial.map((order) => {
-      if (order._id === id) {
-        return {
-          ...order,
-          done: !order.done,
-        };
-      }
-      return order;
-    });
+   return (
+      <div className="flex md:flex-row ">
+         <SideBar />
+         <div className="w-full flex-col ">
+            <div className="justify-center flex lg:mt-16 md:mt-16 mb-20 mt-20 ">
+               <p className="md:text-6xl text-xl text-center font-semibold text-sky-950">
+                  إدارة الطلبات{" "}
+               </p>
+            </div>
+            {loggedIn ? (
+               <div className="flex-grow p-4">
+                  <table className="table-auto w-full">
+                     <thead>
+                        <tr>
+                           <th className="px-4 py-2">المستخدم</th>
+                           <th className="px-4 py-2">اسم المنتج</th>
+                           <th className="px-4 py-2">الخدمة</th>
 
-    setOrderSocial(updatedOrders);
-  };
+                           <th className="px-4 py-2">السعر</th>
+                           <th className="px-4 py-2">تاريخ الطلب</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        {sortedOrders.map((order) => (
+                           <React.Fragment key={order._id}>
+                              <tr
+                                 className={`border-b border-gray-200 cursor-pointer ${
+                                    expandedRow === order._id
+                                       ? "bg-sky-200 font-bold "
+                                       : ""
+                                 } ${order.done ? "bg-blue-300" : ""} ${
+                                    OrderSocial.find(
+                                       (o) => o?._id === order._id
+                                    )
+                                       ? " bg-green-200"
+                                       : "bg-white"
+                                 }`}
+                                 onClick={() => handleRowClick(order._id)}
+                              >
+                                 <td className="px-4 py-2">
+                                    <Link
+                                       href={`#${order.userId}`}
+                                       className="text-blue-500 hover:underline"
+                                    >
+                                       {order.username}
+                                    </Link>
+                                 </td>
+                                 <td className="px-4 py-2">
+                                    <Link
+                                       href={`/services/${order.productName}`}
+                                       className="text-blue-500 hover:underline"
+                                    >
+                                       {order.productName}
+                                    </Link>
+                                 </td>
+                                 <td className="px-4 py-2">
+                                    <Link
+                                       href={`/services/${order.productName}`}
+                                    >
+                                       {order.categoryName || order.linkpost}
+                                    </Link>
+                                 </td>
 
-  const Orders = [...OrderData, ...OrderSocial];
-  const sortedOrders = Orders.sort((a, b) => {
-    const aTime =
-      OrderData.find((order) => order._id === a._id)?.createdTime ||
-      OrderSocial.find((order) => order._id === a._id)?.createdTime;
-    const bTime =
-      OrderData.find((order) => order._id === b._id)?.createdTime ||
-      OrderSocial.find((order) => order._id === b._id)?.createdTime;
-    return new Date(bTime) - new Date(aTime);
-  });
-
-  return (
-    <div className="flex md:flex-row ">
-      <SideBar />
-      <div className="w-full flex-col ">
-        <div className="justify-center flex lg:mt-16 md:mt-16 mb-20 mt-20 ">
-          <p className="md:text-6xl text-xl text-center font-semibold text-sky-950">
-            إدارة الطلبات{" "}
-          </p>
-        </div>
-        {loggedIn ? (
-          <div className="flex-grow p-4">
-            <table className="table-auto w-full">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2">المستخدم</th>
-                  <th className="px-4 py-2">اسم المنتج</th>
-                  <th className="px-4 py-2">الخدمة</th>
-
-                  <th className="px-4 py-2">السعر</th>
-                  <th className="px-4 py-2">تاريخ الطلب</th>
-                </tr>
-              </thead>
-              <tbody>
-                {sortedOrders.map((order) => (
-                  <React.Fragment key={order._id}>
-                    <tr
-                      className={`border-b border-gray-200 cursor-pointer ${
-                        expandedRow === order._id ? "bg-sky-200 font-bold " : ""
-                      } ${order.done ? "bg-blue-300" : ""} ${
-                        OrderSocial.find((o) => o?._id === order._id)
-                          ? " bg-green-200"
-                          : "bg-white"
-                      }`}
-                      onClick={() => handleRowClick(order._id)}
-                    >
-                      <td className="px-4 py-2">
-                        <Link
-                          href={`#${order.userId}`}
-                          className="text-blue-500 hover:underline"
-                        >
-                          {order.username}
-                        </Link>
-                      </td>
-                      <td className="px-4 py-2">
-                        <Link
-                          href={`/services/${order.productName}`}
-                          className="text-blue-500 hover:underline"
-                        >
-                          {order.productName}
-                        </Link>
-                      </td>
-                      <td className="px-4 py-2">
-                        <Link href={`/services/${order.productName}`}>
-                          {order.categoryName}
-                        </Link>
-                      </td>
-
-                      <td className="px-4 py-2">{order.price}</td>
-                      <td className="px-4 py-2">
-                        {" "}
-                        {new Date(order.createdTime).toLocaleDateString()}
-                      </td>
-                      <td className="px-4 py-2">{order.orderTime}</td>
-                      {order.category !== "cards" && (
-                        <td className="px-4 py-2">
-                          {OrderSocial.find((o) => o?._id === order._id) ? (
-                            <input
-                              type="checkbox"
-                              checked={order.done}
-                              onChange={(e) =>
-                                handleCheckboxClick(e, order._id)
-                              }
-                            />
-                          ) : (
-                            <></>
-                          )}
-                        </td>
-                      )}
-                    </tr>
-                  </React.Fragment>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ) : (
-          <Adminlogin login={submitLogin} />
-        )}
+                                 <td className="px-4 py-2">{order.price}</td>
+                                 <td className="px-4 py-2">
+                                    {" "}
+                                    {new Date(
+                                       order.createdTime
+                                    ).toLocaleDateString()}
+                                 </td>
+                                 <td className="px-4 py-2">
+                                    {order.orderTime}
+                                 </td>
+                                 {order.category !== "cards" && (
+                                    <td className="px-4 py-2">
+                                       {OrderSocial.find(
+                                          (o) => o?._id === order._id
+                                       ) ? (
+                                          <input
+                                             type="checkbox"
+                                             checked={order.done}
+                                             onChange={(e) =>
+                                                handleCheckboxClick(
+                                                   e,
+                                                   order._id
+                                                )
+                                             }
+                                          />
+                                       ) : (
+                                          <></>
+                                       )}
+                                    </td>
+                                 )}
+                              </tr>
+                           </React.Fragment>
+                        ))}
+                     </tbody>
+                  </table>
+               </div>
+            ) : (
+               <Adminlogin login={submitLogin} />
+            )}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Orders;
--- components/adminlogin.jsx
+++ components/adminlogin.jsx
@@ -7,118 +7,121 @@ import Logo from "@pics/icons/Logo.png";
 import styles from "@styles/Form.module.css";
 
 const AdminLogin = ({ login }) => {
-  const [buttonClicked, setButtonClicked] = useState(false);
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
+   const [buttonClicked, setButtonClicked] = useState(false);
+   const [password, setPassword] = useState("");
+   const [email, setEmail] = useState("");
+   const [error, setError] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonClicked(true);
-    console.log(email);
-    console.log(password);
-    console.log("Admin email from env:", process.env.AdminEmail);
-    console.log("Admin password from env:", process.env.AdminPassword);
-    if (!email || !password) {
-      setError("All fields are necessary.");
-      setButtonClicked(false);
-      return;
-    }
-
-    try {
-      // Assuming process.env.AdminEmail and process.env.AdminPassword are set correctly
-      if (email == "metiriabdou@gmail.com" && password == "abdou1830") {
-        login();
-        // Perform your logic for successful login
-        console.log("Login successful");
-      } else {
-        setError("Invalid credentials");
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      setButtonClicked(true);
+      console.log(email);
+      console.log(password);
+      console.log("Admin email from env:", process.env.AdminEmail);
+      console.log("Admin password from env:", process.env.AdminPassword);
+      if (!email || !password) {
+         setError("All fields are necessary.");
+         setButtonClicked(false);
+         return;
       }
-    } catch (error) {
-      console.error("Error during login: ", error);
-    } finally {
-      setButtonClicked(false);
-    }
-  };
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 fontgl">
-      <div className="flex items-center justify-center mb-2">
-        <Link href={"/"}>
-          <Image src={Logo} alt="logo" />
-        </Link>
+      try {
+         // Assuming process.env.AdminEmail and process.env.AdminPassword are set correctly
+         if (email == "admin" && password == "admin") {
+            login();
+            // Perform your logic for successful login
+            console.log("Login successful");
+         } else {
+            setError("Invalid credentials");
+         }
+      } catch (error) {
+         console.error("Error during login: ", error);
+      } finally {
+         setButtonClicked(false);
+      }
+   };
+
+   return (
+      <div className="flex flex-col items-center justify-center py-2 fontgl">
+         <div className="flex items-center justify-center mb-2">
+            <Link href={"/"}>
+               <Image src={Logo} alt="logo" />
+            </Link>
+         </div>
+         <h1
+            className="text-4xl font-bold mb-4 text-center text-blue-500"
+            id="menu"
+         >
+            تسجيل
+         </h1>
+
+         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full sm:w-1/2">
+            <div>
+               <label
+                  className="block text-gray-700 font-bold mb-2"
+                  htmlFor="email"
+               >
+                  البريد الإلكتروني
+               </label>
+               <div
+                  className={`${styles.input_group} ${
+                     error && !email ? "border-rose-600" : ""
+                  }`}
+               >
+                  <input
+                     type="email"
+                     name="email"
+                     placeholder="البريد الإلكتروني"
+                     className={styles.input_text}
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}
+                     required
+                  />
+                  <span className="icon flex items-center px-4">
+                     <HiAtSymbol size={25} />
+                  </span>
+               </div>
+            </div>
+            <div>
+               <label
+                  className="block text-gray-700 font-bold mb-2"
+                  htmlFor="password"
+               >
+                  كلمة المرور
+               </label>
+               <div
+                  className={`${styles.input_group} ${
+                     error && !password ? "border-rose-600" : ""
+                  }`}
+               >
+                  <input
+                     type="password"
+                     name="password"
+                     placeholder="كلمة المرور"
+                     className={styles.input_text}
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                     required
+                  />
+                  <span className="icon flex items-center px-4">
+                     <HiAtSymbol size={25} />
+                  </span>
+               </div>
+            </div>
+            <div className="input-button">
+               <button
+                  type="submit"
+                  className={`${styles.button} mt-8`}
+                  onClick={handleSubmit}
+                  disabled={buttonClicked}
+               >
+                  {buttonClicked ? "إنتظر قليلا..." : "تسجيل"}
+               </button>
+            </div>
+            {error && <span className="text-rose-500">{error}</span>}
+         </form>
       </div>
-      <h1
-        className="text-4xl font-bold mb-4 text-center text-blue-500"
-        id="menu"
-      >
-        تسجيل عضو جديد
-      </h1>
-
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full sm:w-1/2">
-        <div>
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
-            البريد الإلكتروني
-          </label>
-          <div
-            className={`${styles.input_group} ${
-              error && !email ? "border-rose-600" : ""
-            }`}
-          >
-            <input
-              type="email"
-              name="email"
-              placeholder="البريد الإلكتروني"
-              className={styles.input_text}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <span className="icon flex items-center px-4">
-              <HiAtSymbol size={25} />
-            </span>
-          </div>
-        </div>
-        <div>
-          <label
-            className="block text-gray-700 font-bold mb-2"
-            htmlFor="password"
-          >
-            كلمة المرور
-          </label>
-          <div
-            className={`${styles.input_group} ${
-              error && !password ? "border-rose-600" : ""
-            }`}
-          >
-            <input
-              type="password"
-              name="password"
-              placeholder="كلمة المرور"
-              className={styles.input_text}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <span className="icon flex items-center px-4">
-              <HiAtSymbol size={25} />
-            </span>
-          </div>
-        </div>
-        <div className="input-button">
-          <button
-            type="submit"
-            className={`${styles.button} mt-8`}
-            onClick={handleSubmit}
-            disabled={buttonClicked}
-          >
-            {buttonClicked ? "إنتظر قليلا..." : "تسجيل"}
-          </button>
-        </div>
-        {error && <span className="text-rose-500">{error}</span>}
-      </form>
-    </div>
-  );
+   );
 };
 
 export default AdminLogin;
